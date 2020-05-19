675bb3576ce7e62faf1f45b12f785bd3533da68c
Romain Coupey <schaft115@hotmail.com>
2020-05-19 19:34:06 +0200

    add seed

--- Gemfile
+++ Gemfile
@@ -24,13 +24,15 @@ gem 'jbuilder', '~> 2.7'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
-gem "table_print"
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem "table_print"
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants...'
+
+category = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+rue = ['rue', 'avenue', 'square', 'impasse de', 'boulevard' ]
+
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: "#{rand(1..60)}, #{rue.sample} #{Faker::FunnyName.name}, Paris",
+    phone_number: "01#{Faker::Company.czech_organisation_number}",
+    category: category.sample
+  )
+end
+
+puts 'Creating reviews...'
+
+Restaurant.all.each do |restaurant|
+  rand(0..10).times do
+    restaurant.reviews.build(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5)
+    ).save
+  end
+end
+
+puts 'Done!'
