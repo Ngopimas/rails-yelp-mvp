3e44758c68181b6d4f96aee7a2247469c3f60f7e
Romain Coupey <schaft115@hotmail.com>
2020-05-19 19:48:43 +0200

    add validations to models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  validates :category, inclusion: { in: CATEGORY }
+  validates :category, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :rating, inclusion: 0..5
+  validates :rating, presence: true
+  validates :content, presence: true
+  validates :restaurant, presence: true
 end
